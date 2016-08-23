6d4ecf13a89a0d0ea6edd38c0dab3b6f884fb12d
guo qi <nusguoqi@gmail.com>
2016-08-23 17:17:10 +0800

    show my location in the map

--- app/src/main/java/sg/edu/nus/idmiapp/MarkerActivity.java
+++ app/src/main/java/sg/edu/nus/idmiapp/MarkerActivity.java
@@ -1,13 +1,30 @@
 package sg.edu.nus.idmiapp;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -15,18 +32,42 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MarkerActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+public class MarkerActivity extends FragmentActivity implements
+        OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        GoogleMap.OnMyLocationButtonClickListener {
 
     private ArrayList<HashMap<String, String>> imageSetArray;
+    private GoogleApiClient mGoogleApiClient;
+    private TextView mMessageView;
+
+    // These settings are the same as the settings for the map. They will in fact give you updates
+    // at the maximal rates currently possible.
+    private static final LocationRequest REQUEST = LocationRequest.create()
+            .setInterval(5000)         // 5 seconds
+            .setFastestInterval(16)    // 16ms = 60fps
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marker);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        mMessageView = (TextView) findViewById(R.id.message_text);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
         this.imageSetArray = (ArrayList) getIntent().getSerializableExtra("imageSetArray");
-        if(null != imageSetArray){
+        if (null != imageSetArray) {
             String text = (imageSetArray.get(0)).get("imageName");
             Toast.makeText(this, text, Toast.LENGTH_LONG).show();
         }
@@ -34,24 +75,118 @@ public class MarkerActivity extends FragmentActivity implements OnMapReadyCallba
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
     public void onMapReady(GoogleMap map) {
-        // Add a marker in the map
-        if(null == this.imageSetArray){
-            return ;
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+            map.setOnMyLocationButtonClickListener(this);
+        } else {
+            Toast.makeText(this, "No Permission", Toast.LENGTH_LONG).show();
         }
-        for(HashMap<String, String> item : this.imageSetArray){
+
+
+        // Add a marker in the map
+        if (null == this.imageSetArray) {
+            return;
+        }
+        for (HashMap<String, String> item : this.imageSetArray) {
             LatLng latLng = new LatLng(Double.parseDouble(item.get("latitude")), Double.parseDouble(item.get("longitude")));
             map.addMarker(new MarkerOptions().position(latLng).title(item.get("imageName")));
         }
+
         map.setOnMarkerClickListener(this);
         LatLng latLng = new LatLng(Double.parseDouble(this.imageSetArray.get(0).get("latitude")), Double.parseDouble(this.imageSetArray.get(0).get("longitude")));
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
 
     }
+
+
+    /**
+     * Button to get current Location. This demonstrates how to get the current Location as required
+     * without needing to register a LocationListener.
+     */
+    public void showMyLocation(View view) {
+        if (mGoogleApiClient.isConnected()) {
+            String msg = "Location = "
+                    + LocationServices.FusedLocationApi;
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+//            getLastLocation(mGoogleApiClient)
+        }
+    }
+
+    /**
+     * Implementation of {@link LocationListener}.
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        mMessageView.setText("Location = " + location);
+    }
+
+    /**
+     * Callback called when connected to GCore. Implementation of {@link GoogleApiClient.ConnectionCallbacks}.
+     */
+    @Override
+
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient,
+                REQUEST,
+                this);  // LocationListener
+    }
+
+    /**
+     * Callback called when disconnected from GCore. Implementation of {@link GoogleApiClient.ConnectionCallbacks}.
+     */
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // Do nothing
+    }
+
+    /**
+     * Implementation of {@link GoogleApiClient.OnConnectionFailedListener}.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Do nothing
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
     @Override
     public boolean onMarkerClick(Marker marker) {
         Toast.makeText(this,marker.getId(),Toast.LENGTH_LONG).show();
         Intent intent = new Intent();
+
         intent.putExtra("imageName", marker.getTitle());
         intent.setClass(this,ShowImageByLocation.class);
         startActivity(intent);
